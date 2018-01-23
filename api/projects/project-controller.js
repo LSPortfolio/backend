b2993440343fc32d02070d2e1bba5649a1b7ae62
Joram Clervius <jclervius@LT-C02N7HV1G3QJ.local>
2018-01-23 01:05:33 -0500

    Some of them done.

--- api/projects/project-controller.js
+++ api/projects/project-controller.js
@@ -11,8 +11,30 @@ module.exports = {
   all: (res, res) => {
     Project.find().then(projects => projects.length > 0 ? res.json(projects) : 'handle errors here', e => 'handle errros here');
   },
-  addComment: (req, res) => {},
-  removeComment: (req, res) => {},
+  addComment: (req, res) => {
+    Project.findByIdAndUpdate(req.params.id,
+      { $set: { comments: req.body.comment }},
+      { new: true, safe: true, upsert: true},
+      (err, response) => {
+        if (err) return handleErr(res, 500);
+      });
+  },
+  removeComment: (req, res) => {
+    Project.findOne(req.params.project, (err,project) => {
+      if (err) return handleErr(res, 500);
+      if (!project) return handleErr(res, 404, 'This project cannot be found.');
+      for (let i = 0; i < project.comments.length; i++) {
+        if (project.comments[i]._id === req.params.commentId) {
+          if (project.comments[i].author !== req.userId && req.role !== 'admin') return handleErr(res, 403, 'You are not authorized to remove this comment');
+          project.comments.splice(i,1);
+        }
+      }
+      project.save((error, updated) => {
+        if (error) return handleErr(res, 500);
+        res.json(updated);
+      })
+    })
+  },
   editComment: (req, res) => {},
   // When users search by tags, or click on a tag.
   tagSearch: (req, res) => {},
@@ -20,17 +42,65 @@ module.exports = {
   createDraft: (req, res) => {},
   makeLive: (req, res) => {
     // make sure the project shows up on contributors' accounts.
+
   },
   makeHidden: (req, res) => {
     // make sure to account for the fact that the project still shows up on contributors' accounts, remove them.
+    Project.findByIdAndUpdate(req.params.id,
+      { $set: { live: false }},
+      { new: true, safe: true, upsert: true },
+      (err, response) => {
+        if (err) return handleErr(res, 500);
+        res.json(response);
+      });
   },
-  addMedia: (req, res) => {},
-  removeMedia: (req, res) => {},
+  addMedia: (req, res) => {
+    Project.findByIdAndUpdate(req.params.id,
+      { $push: { media: req.body.media }},
+      { safe: true, new: true, upsert: true },
+      (err, response) => {
+        if (err) return handleErr(res, 500);
+        res.json(response);
+      });
+  },
+  removeMedia: (req, res) => {
+    Project.findByIdAndUpdate(req.params.projectId, 
+      { $pull: { media: { _id: req.params.mediaId }}},
+      { new: true, safe: true, upsert: true },
+      (err, response) => {
+        if (err) return handleErr(res,500);
+        res.json(response);
+      });
+  },
   updateCover: (req, res) => {},
   updateCategory: (req, res) => {},
-  updateDescription: (req, res) => {},
-  updateName: (req, res) => {},
-  addContributor: (req, res) => {},
+  updateDescription: (req, res) => {
+    Project.findByIdAndUpdate(req.params.id,
+      { $set: { description: req.body.description }},
+      { new: true, upsert: true, safe: true },
+      (err, response) => {
+        if (err) return handleErr(res, 500);
+        res.json(response);
+      })
+  },
+  updateName: (req, res) => {
+    Project.findByIdAndUpdate(req.params.id,
+      { $set: { projectName: req.body.projectName }},
+      { new: true, safe: true, upsert: true },
+      (err, response) => {
+        if (err) return handleErr(res, 500);
+        res.json(response);
+      });
+  },
+  addContributor: (req, res) => {
+    Project.findByIdAndUpdate(req.params.id,
+      { $push: { contributors: req.body.contributor }},
+      { new: true, safe: true, upsert: true },
+      (err, response) => {
+        if (err) return handleErr(res, 500);
+        res.json(response);
+      });
+  },
   removeContributor: (req, res) => {},
   saveDraft: (req, res) => {
     Project.findByIdAndUpdate(req.params.id, 
@@ -42,5 +112,5 @@ module.exports = {
       })
   },
   // Admin only controllers.
-  deleteProject: () => {},
+  deleteProject: (req, res) => Project.findByIdAndRemove(req.params.id, (err, response) => err ? handleErr(res, 500) : res.json(response)),
 }
--- api/util/index.js
+++ api/util/index.js
@@ -10,59 +10,59 @@ const async = require('async');
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API }).base(process.env.AIRTABLE_BASE);
 
 winston.add(winston.transports.Loggly, {
-    token: process.env.LOGGY_TOKEN,
-    subdomain: 'lambda_showcase',
-    tags: ['winston-nodejs'],
-    json: true
+	token: process.env.LOGGY_TOKEN,
+  subdomain: 'lambda_showcase',
+  tags: ['winston-nodejs'],
+  json: true
 });
 
 const emailUser = (type, to, subject, body) => {
-    const from = {
-        name: "Lambda School",
-        email: "hello@lambdashowcase.com"
-    }
-    return new Promise((resolve, reject) => {
-        const msg = {
-            to,
-            from,
-            subject: type.subject || subject,
-            html: type.html || body,
-            //TODO: Get tpl id
-            templateId: ''
-        };
-        sgMail.send(msg, (err, result) => err ? reject(err) : resolve(result));
-    })
+  const from = {
+  name: "Lambda School",
+    email: "hello@lambdashowcase.com"
+  }
+  return new Promise((resolve, reject) => {
+    const msg = {
+      to,
+      from,
+      subject: type.subject || subject,
+      html: type.html || body,
+      //TODO: Get tpl id
+      templateId: ''
+    };
+    sgMail.send(msg, (err, result) => err ? reject(err) : resolve(result));
+  })
 };
 
 module.exports = {
-    handleErr: (res, status, message, data) => {
-        winston.log("Site error", `${status} - ${message}: ${data}`);
-        return status === 500 ? res.status(500).send({ message: 'Server error with this operation.' }) : res.status(status).send({ message });
-    },
-    isLoggedIn: (req, res, next) => {
-        const message = "You are not authorized to perform this operation.";
-        const token = req.body.token || req.query.token || req.headers['x-access-token'];
-        if (!token) return res.status(403).send({ message });
-        jwt.verify(token, process.env.SECRET, (err, decoded) => {
-            if (err) return res.status(403).send({ message });
-						req.decoded = decoded;
-						req.userId = decoded.id;
-            next();
-        });
-    },
-    isPermitted: (req, res, next) => {
-        const message = "Your account is not permitted to perform this operation.";
-        const { permitted } = req.decoded;
-        if (!permitted) return res.status(403).send({ message });
-        next();
-    },
-    isAdmin: (req, res, next) => {
-        const message = "You must be an admin to perform this operation.";
-        const { role } = req.decoded;
-        if (role !== "staff" && role !== "admin") return res.status(403).send({ message });
-        next();
-    },
-    checkAirTableRoles = (email, role) => {
+  handleErr: (res, status, message, data) => {
+    winston.log("Site error", `${status} - ${message}: ${data}`);
+    return status === 500 ? res.status(500).send({ message: 'Server error with this operation.' }) : res.status(status).send({ message });
+  },
+  isLoggedIn: (req, res, next) => {
+    const message = "You are not authorized to perform this operation.";
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (!token) return res.status(403).send({ message });
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err) return res.status(403).send({ message });
+			req.decoded = decoded;
+			req.userId = decoded.id;
+      next();
+    });
+  },
+  isPermitted: (req, res, next) => {
+    const message = "Your account is not permitted to perform this operation.";
+    const { permitted } = req.decoded;
+    if (!permitted) return res.status(403).send({ message });
+    next();
+  },
+  isAdmin: (req, res, next) => {
+    const message = "You must be an admin to perform this operation.";
+    const { role } = req.decoded;
+    if (role !== "staff" && role !== "admin") return res.status(403).send({ message });
+    next();
+  },
+  checkAirTableRoles = (email, role) => {
 			let emailFound = false;
 			const table = role === 'student' ? 'Table 1' : 'Table 2';
 			const checkOnly = () => {
@@ -83,33 +83,33 @@ module.exports = {
 					})
 				});
 			}  
-      return new Promise((resolve, reject) => {
-        Permitted.findOne({ email }).exec()
-          .then(permitted => {
-						if (permitted) return resolve();
-						base(table).select({
-							maxRecords: 100
-						}).eachPage(function page(records, fetchNextPage) {			
-							async.each(records, checkAndAdd, err => {
-								if (err) return reject(err);
-								if (emailFound || checkOnly()) return resolve();
-								return records.length < 100 ? reject() : fetchNextPage();
-							});
-            }, function done(err) {
-              if (err) return reject(err)
-          	})
+    return new Promise((resolve, reject) => {
+    Permitted.findOne({ email }).exec()
+      .then(permitted => {
+				if (permitted) return resolve();
+				base(table).select({
+					maxRecords: 100
+				}).eachPage(function page(records, fetchNextPage) {			
+					async.each(records, checkAndAdd, err => {
+						if (err) return reject(err);
+						if (emailFound || checkOnly()) return resolve();
+						return records.length < 100 ? reject() : fetchNextPage();
+				});
+      }, function done(err) {
+        if (err) return reject(err)
+      	})
 					}, e => reject(e))
-          .catch(error => reject(error))
-      })
-    },
-    sendEmail: {
-			welcome: to => emailUser(emails.welcome, to),
-			forgotPassword: (to, token) => {
-				const type = {
-					subject: emails.resetPassword.subject,
-					html: emails.resetPassword.html(token)
-				}
-				return emailUser(type, to);
+      .catch(error => reject(error))
+    })
+  },
+  sendEmail: {
+		welcome: to => emailUser(emails.welcome, to),
+		forgotPassword: (to, token) => {
+			const type = {
+				subject: emails.resetPassword.subject,
+				html: emails.resetPassword.html(token)
 			}
-    }
+			return emailUser(type, to);
+		}
+  }
 }
