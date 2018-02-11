3b1bf7e559e6b44096d0c7f2dde6aa145f374c33
Roland Canuto <rolandcanuto@outlook.com>
2018-02-11 00:25:26 -0800

    heroku test number over 9000

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const port = process.env.PORT || 3030;
 const app = require('express')();
 const config = require('./config/config');
 
@@ -7,6 +8,6 @@ require('./config/express')(app,config);
 require('./config/mongoose')(config);
 require('./config/routes')(app);
 
-app.listen(config.port, () => {
+app.listen(port, () => {
   console.log('The Lambda School Portfolio server is up and running.');
 });
