d699e3b82ad0787958d71f6215a5091f848c084d
Roland Canuto <rolandcanuto@outlook.com>
2018-02-11 22:51:57 -0800

    server test

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,5 @@
 module.exports = app => {
-  const blankRoute = (req, res) => res.json({ test: 'If you see this that the server is not working, but you have not specified a route'});
-  app.get('/helloThere', (req, res) => {
-    res.json(process.env.DATABASE);
-  });
+  const blankRoute = (req, res) => res.json({ test: 'If you see this that the server is working, but you have not specified a route'});
 
   app.use('/user', require('../api/user'));
   app.use('/project', require('../api/projects'));
