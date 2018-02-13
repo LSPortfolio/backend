e4e65e40abdcf8a3ae0a0326c1706d094765dfde
Roland <rolandcanuto@outlook.com>
2018-02-13 11:19:05 -0800

    added names to contributor

--- api/projects/project-model.js
+++ api/projects/project-model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
+  name: String,
   author: {
     type: Schema.Types.ObjectId,
     ref: 'User'
