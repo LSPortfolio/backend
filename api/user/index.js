cf9472acaa5e74f45bfccf6d6ddc30030ce310ab
Roland <rolandcanuto@outlook.com>
2018-02-14 09:55:48 -0800

    forgot to put endpoints

--- api/user/index.js
+++ api/user/index.js
@@ -4,7 +4,9 @@ const controller = require('./user-controller');
 
 router.post('/create', controller.createUser);
 router.post('/login', controller.login);
+
 router.put('/forgotPassword', controller.forgotPassword);
+router.put('/resetPassword', controller.resetPassword);
 router.put('/find', controller.find);
 
 module.exports = router;
